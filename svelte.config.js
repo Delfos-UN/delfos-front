87cf0bfb84bd60a7ad6e49fad2ee7c8cf8c9a958
Juan Durán <sebas2812.jd@gmail.com>
2024-10-19 01:02:47 -0500

    fix: Configuraciones de Docker

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from 'svelte-adapter-bun';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
- preprocess: vitePreprocess(),
- kit: {
-  adapter: adapter()
- }
+  preprocess: sveltePreprocess(),
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;
